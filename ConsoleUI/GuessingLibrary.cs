568d338fec3db647aa675a297e63bc3b74afbea7
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-18 15:53:32 +0200

    Added library routines
    Routines handle the operations of guessing the number

---
+++ ConsoleUI/GuessingLibrary.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class GuessingLibrary
+    {
+        public static void WelcomeToGame()
+        {
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+            Console.WriteLine("====================================");
+        }
+
+        public static string GetUserInput()
+        {
+            string input = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(input) == true)
+            {
+                return "";
+            }
+            else
+            {
+                return input;
+            }
+        }
+        public static int GetDifficultyChoice()
+        {
+            Console.WriteLine("Enter difficulty choice (1-easy, 2-normal 3-difficult): ");
+            string difficulty = GetUserInput();
+
+            if (String.IsNullOrEmpty(difficulty) == false)
+            {
+                if (int.TryParse(difficulty, out int choice) == true)
+                {
+                    return choice;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static (int range, int numberOfTries, int chosenNumber) SetupGame(int choice)
+        {
+            Random random = new Random();
+            if (choice > 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        return (10, 5, random.Next(10));
+                    case 2:
+                        return (100, 3, random.Next(100));
+                    case 3:
+                        return (1000, 2, random.Next(1000));
+                    default:
+                        return (-1, -1, -1);
+                }
+            }
+            else
+            {
+                return (-1, -1, -1);
+            }
+        }
+
+        public static int GetGuess(int range, int numberOfTries)
+        {
+            Console.WriteLine($"Enter a number between {0} and {range} ({numberOfTries} tries remaining)");
+            string guess = GetUserInput();
+
+            if (int.TryParse(guess, out int number) == true)
+            {
+                return number;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static string GetVarianceMessage(int guess, int chosenNumber)
+        {
+            if (guess > chosenNumber)
+            {
+                return "Too High";
+            }
+            else
+            {
+                return "Too Low";
+            }
+        }
+
+        public static void PlayGame(int range, int numberOfTries, int chosenNumber)
+        {
+            do
+            {
+                int guess = GetGuess(range, numberOfTries);
+
+                if (guess == chosenNumber)
+                {
+                    Console.WriteLine("You won!!!");
+                    break;
+                }
+                else
+                {
+                    string varianceMessage = GetVarianceMessage(guess, chosenNumber);
+                    Console.WriteLine($"Your guess is {varianceMessage}");
+                    numberOfTries--;
+                }
+
+            } while (numberOfTries != 0);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            /* 
+             Get difficultly level choice from user
+
+            Based on difficultly, set number to be guessed and set number of tries
+
+            let user know how many tries
+
+            do
+	            Get number from user
+	            convert to int
+            while less than tries
+             */
+
+            GuessingLibrary.WelcomeToGame();
+
+            int difficultyLevel = GuessingLibrary.GetDifficultyChoice();
+
+            if (difficultyLevel > 0)
+            {
+                var (range, numberOfTries, chosenNumber) = GuessingLibrary.SetupGame(difficultyLevel);
+                if (range > 0 && numberOfTries > 0 && chosenNumber > 0)
+                {
+                    GuessingLibrary.PlayGame(range, numberOfTries, chosenNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid level selected");
+            }
+
         }
     }
 }
